0e401b4aada346576ad4047a7790f546679eda26
Zygnius <twjia1998@gmail.com>
2016-10-16 17:05:22 -0400

    gitignore update

--- Love on Rails/Assets/Scripts/NextRoom.cs
+++ Love on Rails/Assets/Scripts/NextRoom.cs
@@ -5,11 +5,46 @@ using UnityEngine.UI;
 public class NextRoom : MonoBehaviour {
 
     public Image objectFound;
+    public Image dimImage;
+    public Text displayText;
+    private bool hit = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("hit");
+        Debug.Log("Next room trigger hit");
         objectFound.canvasRenderer.SetAlpha(255);
+        hit = true;
+    }
+
+    void Update()
+    {
+        if (hit)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                StartCoroutine(dim());
+                hit = false;
+            }
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            objectFound.canvasRenderer.SetAlpha(255);
+            hit = true;
+        }
+        
+        
+    }
+
+    IEnumerator dim()
+    {
+        float mult = 10f;
+        while (dimImage.canvasRenderer.GetAlpha() < 255)
+        {
+            yield return new WaitForSeconds(0.05f);
+            dimImage.canvasRenderer.SetAlpha(mult);
+            mult += 10f;
+        }
+        
     }
 
 }
