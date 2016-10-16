6ab1df2c831e86a2939c6d0074b085b30623b129
Zygnius <twjia1998@gmail.com>
2016-10-16 14:25:54 -0400

    Aspect Ratio and Next Room Trigger

---
+++ Love on Rails/Assets/Scripts/NextRoom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NextRoom : MonoBehaviour {
+
+    public Image objectFound;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hit");
+        objectFound.canvasRenderer.SetAlpha(255);
+    }
+
+}
